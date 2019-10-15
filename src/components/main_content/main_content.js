4fc5a3accb0364f47df968eb7e1eb038a2f09f41
fluffychacham <chamtejido@gmail.com>
2019-10-15 00:26:09 -0600

    added Nav, MainContent, Footer components

---
+++ src/components/main_content/main_content.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./main_content.css";
+
+export default class MainContent extends Component {
+  render() {
+    return <div>This is main content</div>;
+  }
+}
---
+++ src/components/nav/nav.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./nav.css";
+
+export default class Nav extends Component {
+  render() {
+    return <div>This is nav</div>;
+  }
+}
